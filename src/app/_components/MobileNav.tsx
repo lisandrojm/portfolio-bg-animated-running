ae1ef33a6b486c92e3e532f1fe34323d56c0eb14
lisandrojm <lisandrojm@gmail.com>
2023-12-26 18:10:32 -0300

    refactoring

--- src/app/_components/MobileNav.tsx
+++ src/app/_components/MobileNav.tsx
@@ -31,7 +31,7 @@ export default function MobileNav() {
       </button>
       <div className={`fixed left-0 top-0 z-10 h-full w-full transform inset-0 backdrop-blur-lg border-l-[1px] border-orange duration-300 ease-in-out ${navShow ? 'translate-x-0' : 'translate-x-full'}`}>
         <div className="flex justify-end">
-          <button className="mr-6 mt-10 h-8 w-8" aria-label="Toggle Menu" onClick={onToggleNav}>
+          <button className="mr-6 mt-8 h-8 w-8" aria-label="Toggle Menu" onClick={onToggleNav}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="text-white">
               <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
             </svg>
